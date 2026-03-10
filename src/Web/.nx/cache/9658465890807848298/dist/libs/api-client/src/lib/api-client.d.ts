7e35f73743ddbcadfd1f8e53ac9f104162511dd7
Filipe Frigini <filipef@betenbough.com>
2026-03-10 11:30:31 -0300

    fix(git): remove .nx cache and temporary artifacts from tracking and harden .gitignore

--- src/Web/.nx/cache/9658465890807848298/dist/libs/api-client/src/lib/api-client.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function apiClient(): string;
--- src/Web/.nx/cache/9658465890807848298/dist/libs/api-client/src/lib/api-client.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.apiClient = apiClient;
-function apiClient() {
-    return 'api-client';
-}
-//# sourceMappingURL=api-client.js.map
