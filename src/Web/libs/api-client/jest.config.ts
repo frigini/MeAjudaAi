20e1e3de1c851a1dfb1e96f3025286d08afa74aa
Filipe Frigini <filipef@betenbough.com>
2026-03-09 22:32:52 -0300

    feat: create new api-client library and its associated Nx workspace configuration files.

--- src/Web/libs/api-client/jest.config.ts
+++ src/Web/libs/api-client/jest.config.ts
@@ -1,9 +1,15 @@
-module.exports = {
+export default {
   displayName: 'api-client',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+    '^.+\\.[tj]sx?$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        isolatedModules: true
+      }
+    ]
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/libs/api-client',
