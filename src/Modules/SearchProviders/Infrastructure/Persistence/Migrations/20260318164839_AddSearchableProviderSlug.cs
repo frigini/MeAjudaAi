76d4ae91e1727f5b382cace4254f411561f9cac2
Filipe Frigini <filipef@betenbough.com>
2026-03-18 13:53:20 -0300

    feat: Add slug field to Provider and SearchableProvider entities.

---
+++ src/Modules/SearchProviders/Infrastructure/Persistence/Migrations/20260318164839_AddSearchableProviderSlug.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MeAjudaAi.Modules.SearchProviders.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSearchableProviderSlug : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
