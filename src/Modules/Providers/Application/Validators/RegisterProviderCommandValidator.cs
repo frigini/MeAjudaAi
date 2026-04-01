c0bb339f629d043e5f8ac03183d2ef9a94edf9e5
Filipe Frigini <filipef@betenbough.com>
2026-04-01 19:58:14 -0300

    fix(providers): add email and phone validation to RegisterProviderCommandValidator

--- src/Modules/Providers/Application/Validators/RegisterProviderCommandValidator.cs
+++ src/Modules/Providers/Application/Validators/RegisterProviderCommandValidator.cs
@@ -18,10 +18,14 @@ public class RegisterProviderCommandValidator : AbstractValidator<RegisterProvid
             .NotEmpty().WithMessage("O número do documento é obrigatório")
             .MaximumLength(20).WithMessage("O número do documento não pode exceder 20 caracteres");
 
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("O e-mail é obrigatório")
+            .EmailAddress().WithMessage("E-mail inválido");
+
         RuleFor(x => x.PhoneNumber)
+            .NotEmpty().WithMessage("O número de telefone é obrigatório")
             .MaximumLength(20).WithMessage("O número de telefone não pode exceder 20 caracteres")
-            .Matches(@"^\+?[1-9]\d{1,14}$").WithMessage("Número de telefone inválido")
-            .When(x => !string.IsNullOrEmpty(x.PhoneNumber));
+            .Matches(@"^\+?[1-9]\d{1,14}$").WithMessage("Número de telefone inválido");
 
         RuleFor(x => x.Type)
             .IsInEnum().WithMessage("Tipo de prestador inválido");
